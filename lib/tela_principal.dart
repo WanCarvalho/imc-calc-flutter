43ce17e5862b5f0505c4418fb120666a12bf25a4
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-10-13 11:50:59 -0300

    feat: adicionando componente para cards

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -25,60 +25,47 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFF9E9E9E),
-                    ),
-                  ),
+                  child: CardPadrao(cor: const Color(0xFFE9E9E9))
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFF9E9E9E),
-                    ),
-                  ),
+                  child: CardPadrao(cor: const Color(0xFFE9E9E9))
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color(0xFF9E9E9E),
-              ),
-            ),
+            child: CardPadrao(cor: const Color(0xFFE9E9E9))
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFF9E9E9E),
-                    ),
-                  ),
+                  child: CardPadrao(cor: const Color(0xFFE9E9E9))
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFF9E9E9E),
-                    ),
-                  ),
+                  child: CardPadrao(cor: const Color(0xFFE9E9E9)),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CardPadrao extends StatelessWidget {
+  CardPadrao({required this.cor});
+  
+  Color cor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: cor,
       ),
     );
   }
