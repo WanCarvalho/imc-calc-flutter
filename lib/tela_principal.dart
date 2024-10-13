6b7e2e996b1a1da5231e048f68a88a11e7c2cb83
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-10-13 18:26:01 -0300

    feat: adicionando logica para selecionar sexo

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -7,12 +7,38 @@ const alturaContainerInferior = 80.0;
 const corCardPadrao = Color(0xFFE9E9E9);
 const corContainerInferior = Color(0xFFFF5822);
 
+const corMasculinoInativaCardPadrao = Colors.lightBlueAccent;
+const corFemininoInativaCardPadrao = const Color.fromARGB(255, 236, 87, 137);
+
 class TelaPrincipal extends StatefulWidget {
   @override
   _TelaPrincipalState createState() => _TelaPrincipalState();
 }
 
 class _TelaPrincipalState extends State<TelaPrincipal> {
+  Color corMasculinoCardPadrao = corMasculinoInativaCardPadrao;
+  Color corFemininoCardPadrao = corFemininoInativaCardPadrao;
+
+  // 1 -> masculino
+  // 2 -> feminino
+  void atualizarCor(int sexo) {
+    if (sexo == 1) { // checa se card masculino foi pressionado
+      if (corMasculinoCardPadrao == corMasculinoInativaCardPadrao) {
+        corMasculinoCardPadrao = const Color.fromARGB(255, 22, 138, 192);
+        corFemininoCardPadrao = corFemininoInativaCardPadrao;
+      } else {
+        corMasculinoCardPadrao = corMasculinoInativaCardPadrao;
+      }
+    } else if (sexo == 2) { // checa se card feminino foi pressionado
+      if (corFemininoCardPadrao == corFemininoInativaCardPadrao) {
+        corFemininoCardPadrao = const Color.fromARGB(255, 209, 43, 98);
+        corMasculinoCardPadrao = corMasculinoInativaCardPadrao;
+      } else {
+        corFemininoCardPadrao = corFemininoInativaCardPadrao;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,20 +58,34 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
             child: Row(
               children: [
                 Expanded(
-                  child: CardPadrao(
-                    cor: corCardPadrao,
-                    filhoCartao: ConteudoIcone(
-                      icone: FontAwesomeIcons.mars,
-                      descricao: 'MASCULINO',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        atualizarCor(1);
+                      });
+                    },
+                    child: CardPadrao(
+                      cor: corMasculinoCardPadrao,
+                      filhoCartao: ConteudoIcone(
+                        icone: FontAwesomeIcons.mars,
+                        descricao: 'MASCULINO',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: CardPadrao(
-                    cor: corCardPadrao,
-                    filhoCartao: ConteudoIcone(
-                      icone: FontAwesomeIcons.venus,
-                      descricao: 'FEMININO',
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        atualizarCor(2);
+                      });
+                    },
+                    child: CardPadrao(
+                      cor: corFemininoCardPadrao,
+                      filhoCartao: ConteudoIcone(
+                        icone: FontAwesomeIcons.venus,
+                        descricao: 'FEMININO',
+                      ),
                     ),
                   ),
                 ),
